c385033423900b5c7d4eea32a35381e7b8cfe074
Pedro Silva <pemanuel2@hotmail.com>
2018-04-09 15:59:34 -0300

    Inicio do find file

--- src/MulticastPeer.java
+++ src/MulticastPeer.java
@@ -13,43 +13,48 @@ public class MulticastPeer {
     Random rand = new Random();
     int id = rand.nextInt(50) + 1;
     List<user> knownUsers = new ArrayList<>();
+    List<DatagramPacket> toSendList = new ArrayList<>();
+    InetAddress group;
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws UnknownHostException {
         MulticastPeer myMulti = new MulticastPeer();
+        myMulti.group = InetAddress.getByName("228.5.6.7");
         System.out.println("Eu: id = " + myMulti.id);
         global.estado = 0;
+        //define ip do multicast
 
         MulticastSocket s = null;
         try {
-            //define ip do multicast
-            InetAddress group = InetAddress.getByName("228.5.6.7");
+
             //cria socket
             s = new MulticastSocket(6789);
             //da join no grupo de multicast
-            s.joinGroup(group);
-            //scanner para ler do teclado
-            Scanner sc = new Scanner(System.in);
+            s.joinGroup(myMulti.group);
 
             //cria thread de recepção
             listener c = new listener(s, myMulti);
-
-            //loop  de envio
+            speaker sp = new speaker(s, myMulti);
+            
             while (true) {
-
                 if (global.estado == 0) {
                     String stringToSend = new String("[" + myMulti.id + "]: " + "[New user]" + myMulti.id);
                     //cria um datagram com a mensagem
-                    DatagramPacket messageOut = new DatagramPacket(stringToSend.getBytes(), stringToSend.length(), group, 6789);
+                    DatagramPacket messageOut = new DatagramPacket(stringToSend.getBytes(), stringToSend.length(), myMulti.group, 6789);
                     //manda a mensagem
-                    s.send(messageOut);
+                    myMulti.toSendList.add(messageOut);
                     global.estado = 1;
-                } else if (global.estado == 1) {
+                }
+                //scanner para ler do teclado
+                Scanner sc = new Scanner(System.in);
+                if (global.estado == 1) {
                     //le do teclado e transforma em bytes
-                    byte[] m = sc.nextLine().getBytes();
+                    
+                    String stringToSend = new String("[" + myMulti.id + "]:[Find file]" +  sc.nextLine());
+                    byte[] m =stringToSend.getBytes();
                     //cria um datagram com a mensagem
-                    DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);
+                    DatagramPacket messageOut = new DatagramPacket(m, m.length, myMulti.group, 6789);
                     //manda a mensagem
-                    s.send(messageOut);
+                    myMulti.toSendList.add(messageOut);
 
                 }
 
@@ -94,16 +99,40 @@ class listener extends Thread {
             } catch (IOException ex) {
                 Logger.getLogger(listener.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
-            
+
             String recebida = new String(messageIn.getData());
             if (recebida.contains("[New user]")) {
                 String idString = new String(recebida.split("\\]")[2].trim());
                 Integer newId = Integer.valueOf(idString);
+
                 if (newId != myMulti.id) {
-                    myMulti.knownUsers.add(new user(newId, "", ""));
-                    System.out.println("minha lista é: " + myMulti.knownUsers);
+                    Boolean findEqual = false;
+                    for (user u : myMulti.knownUsers) {
+                        if (u.name == newId) {
+                            findEqual = true;
+                            break;
+                        }
+                    }
+                    if (findEqual == false) {
+                        myMulti.knownUsers.add(new user(newId, "", ""));
+
+                        System.out.println("minha lista é: " + myMulti.knownUsers);
+                        //mandar meu contato para o novo user
+                        String stringToSend = new String("[" + myMulti.id + "]: " + "[New user]" + myMulti.id);
+                        
+                        myMulti.toSendList.add(new DatagramPacket(stringToSend.getBytes(), stringToSend.length(), myMulti.group, 6789));
+                        
+                    }
+
                 }
+            }
+            else if(recebida.contains("[Find file]"))
+            {
+                //System.out.println(recebida);
+                String fileNameString = new String(recebida.split("\\]")[2].trim());
+                String stringToSend = new String("[" + myMulti.id + "]: " + "[File found]" + " eu tenho " + fileNameString);
+                        
+                myMulti.toSendList.add(new DatagramPacket(stringToSend.getBytes(), stringToSend.length(), myMulti.group, 6789));
             }
 
             System.out.println(new String(messageIn.getData()));
@@ -117,4 +146,44 @@ class listener extends Thread {
     public void exit(int status) {
         running = false;
     }
+}
+
+class speaker extends Thread {
+
+    MulticastPeer myMulti;
+    MulticastSocket s;
+
+    public speaker(MulticastSocket socket, MulticastPeer myMulti) {
+        this.myMulti = myMulti;
+        this.s = socket;
+        this.start();
+    }
+
+    public void run() {
+
+        try {
+            //loop  de envio
+            while (true) {
+               // System.out.println(".");
+                sleep(1000);
+                //System.out.println("-");
+                if (myMulti.toSendList.size() > 0) {
+                    //System.out.println("algo para falar");
+                    for (int i = 0; i < myMulti.toSendList.size(); i++) {
+                       // System.out.println("in");
+                        s.send(myMulti.toSendList.remove(0));
+                       // System.out.println("out");
+                    }
+
+                }
+
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(speaker.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(speaker.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
 }
