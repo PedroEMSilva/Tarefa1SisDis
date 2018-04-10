795ddf8b5c32a557dca516ea4b67a301d1f7cd53
Pedro Silva <pemanuel2@hotmail.com>
2018-04-10 17:24:30 -0300

    unicast

--- src/MulticastPeer.java
+++ src/MulticastPeer.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 import java.util.Random;
 
 public class MulticastPeer {
+
     //rand para criar o id 
     Random rand = new Random();
     //criando id (unico) 
@@ -17,14 +18,14 @@ public class MulticastPeer {
     List<user> knownUsers = new ArrayList<>();
     //lista de datagramas para mandar
     List<DatagramPacket> toSendList = new ArrayList<>();
-    
+
     InetAddress group;
 
     public static void main(String args[]) throws UnknownHostException {
         MulticastPeer myMulti = new MulticastPeer();
         myMulti.group = InetAddress.getByName("228.5.6.7");
         System.out.println("Eu: id = " + myMulti.id);
-        
+
         global.estado = 0;
         //define ip do multicast
 
@@ -40,7 +41,9 @@ public class MulticastPeer {
             listener c = new listener(s, myMulti);
             //cria thread de envio
             speaker sp = new speaker(s, myMulti);
-            
+            //cria thread de recepção de unicast
+            listenerUnicast c2 = new listenerUnicast( myMulti);
+
             while (true) {
                 if (global.estado == 0) {
                     String stringToSend = new String("[" + myMulti.id + "]: " + "[New user]" + myMulti.id);
@@ -54,9 +57,9 @@ public class MulticastPeer {
                 Scanner sc = new Scanner(System.in);
                 if (global.estado == 1) {
                     //le do teclado e transforma em bytes
-                    
-                    String stringToSend = new String("[" + myMulti.id + "]:[Find file]" +  sc.nextLine());
-                    byte[] m =stringToSend.getBytes();
+
+                    String stringToSend = new String("[" + myMulti.id + "]:[Find file]" + sc.nextLine());
+                    byte[] m = stringToSend.getBytes();
                     //cria um datagram com a mensagem
                     DatagramPacket messageOut = new DatagramPacket(m, m.length, myMulti.group, 6789);
                     //manda a mensagem
@@ -81,7 +84,6 @@ public class MulticastPeer {
 
 class listener extends Thread {
 
-
     MulticastSocket socket;
     boolean running = true;
     MulticastPeer myMulti;
@@ -95,7 +97,6 @@ class listener extends Thread {
 
     public void run() {
 
-        // an echo server
         DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
 
         while (running) {
@@ -107,9 +108,20 @@ class listener extends Thread {
             }
 
             String recebida = new String(messageIn.getData());
+            String idString = new String(recebida.split("\\]")[0].trim());
+            idString = new String(idString.split("\\[")[1].trim());
+            Integer id = Integer.valueOf(idString);
+            if(id == myMulti.id)
+            {
+                continue;
+            }
+            
+            
+            int port = id+10000;
+            //se falarem no multicast um id, tenho certeza que é um usuário novo portanto vou responder ele por unicast
             if (recebida.contains("[New user]")) {
-                String idString = new String(recebida.split("\\]")[2].trim());
-                Integer newId = Integer.valueOf(idString);
+                String NewIdString = new String(recebida.split("\\]")[2].trim());
+                Integer newId = Integer.valueOf(NewIdString);
 
                 if (newId != myMulti.id) {
                     Boolean findEqual = false;
@@ -120,28 +132,44 @@ class listener extends Thread {
                         }
                     }
                     if (findEqual == false) {
-                        myMulti.knownUsers.add(new user(newId, "", ""));
+                        try {
+                            myMulti.knownUsers.add(new user(newId, "", ""));
+                            
+                            System.out.println("minha lista é: " + myMulti.knownUsers);
+                            //mandar meu contato para o novo user
+                            String stringToSend = new String("[" + myMulti.id + "]: " + "[New user]" + myMulti.id);
+                            int newPort = newId+10000;
+                            
+                            Socket clientSocket = new Socket("localhost", newPort);
+                            
+                            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                             out.println(stringToSend);
+                            
+                            
+                            
+                        } catch (IOException ex) {
+                            Logger.getLogger(listener.class.getName()).log(Level.SEVERE, null, ex);
+                        }
 
-                        System.out.println("minha lista é: " + myMulti.knownUsers);
-                        //mandar meu contato para o novo user
-                        String stringToSend = new String("[" + myMulti.id + "]: " + "[New user]" + myMulti.id);
-                        
-                        myMulti.toSendList.add(new DatagramPacket(stringToSend.getBytes(), stringToSend.length(), myMulti.group, 6789));
-                        
                     }
 
                 }
-            }
-            else if(recebida.contains("[Find file]"))
-            {
-                //System.out.println(recebida);
-                String fileNameString = new String(recebida.split("\\]")[2].trim());
-                
-                
-                //checar se eu tenho arquivo
-                String stringToSend = new String("[" + myMulti.id + "]: " + "[File found]" + " eu tenho " + fileNameString);
-                        
-                myMulti.toSendList.add(new DatagramPacket(stringToSend.getBytes(), stringToSend.length(), myMulti.group, 6789));
+            } else if (recebida.contains("[Find file]")) {    //se alguem ta procurando  o arquivo, tenho que ver se tenho o arquivo e responder por unicast
+                try {
+                    //System.out.println(recebida);
+                    String fileNameString = new String(recebida.split("\\]")[2].trim());
+                    
+                    //checar se eu tenho arquivo
+                    String stringToSend = new String("[" + myMulti.id + "]: " + "[File found]" + " eu tenho " + fileNameString);
+                    
+                    
+                    Socket clientSocket = new Socket("localhost", port);
+                    
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    out.println(stringToSend);
+                } catch (IOException ex) {
+                    Logger.getLogger(listener.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
 
             System.out.println(new String(messageIn.getData()));
@@ -173,15 +201,15 @@ class speaker extends Thread {
         try {
             //loop  de envio
             while (true) {
-               // System.out.println(".");
+                // System.out.println(".");
                 sleep(1000);
                 //System.out.println("-");
                 if (myMulti.toSendList.size() > 0) {
                     //System.out.println("algo para falar");
                     for (int i = 0; i < myMulti.toSendList.size(); i++) {
-                       // System.out.println("in");
+                        // System.out.println("in");
                         s.send(myMulti.toSendList.remove(0));
-                       // System.out.println("out");
+                        // System.out.println("out");
                     }
 
                 }
@@ -192,6 +220,93 @@ class speaker extends Thread {
         } catch (InterruptedException ex) {
             Logger.getLogger(speaker.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+    }
+
+}
+
+class listenerUnicast extends Thread {
+
+    boolean running = true;
+    MulticastPeer myMulti;
+    List<Socket> clientSocketList = new ArrayList<>();
+    byte[] buffer = new byte[1000];
+
+    public listenerUnicast( MulticastPeer myMulti) {
+        this.myMulti = myMulti;
+        this.start();
+    }
+
+    public void run() {
+       
+
+        try {
+            int port = 10000 + myMulti.id;
+            
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            serverSocket.setSoTimeout(1000);
+
+            while (true) {
+                Socket clientSocket = null;
+                try {
+                    clientSocket = serverSocket.accept();
+                } catch (IOException ex) {
+                    //Logger.getLogger(listenerUnicast.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                if (clientSocket != null) {
+                    clientSocket.setSoTimeout(1000);
+                    
+                    clientSocketList.add(clientSocket);
+                }
+                for (Socket s : clientSocketList) {
+                    DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    String recebida = null;
+                    try {
+                       
+                        recebida =in.readLine();
+                    } catch (IOException ex) {
+                       // Logger.getLogger(listenerUnicast.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                    s.close();
+                    
+                    System.out.println("recebida uni :" + recebida);
+                    //Se alguem me mandar por unicast seu id, eu nã presciso responder, mas tenho que ver se ja tenho o contato
+                    if (recebida.contains("[New user]")) {
+                        String idString = new String(recebida.split("\\]")[2].trim());
+                        Integer newId = Integer.valueOf(idString);
+
+                        if (newId != myMulti.id) {
+                            Boolean findEqual = false;
+                            for (user u : myMulti.knownUsers) {
+                                if (u.name == newId) {
+                                    findEqual = true;
+                                    break;
+                                }
+                            }
+                            if (findEqual == false) {
+                                myMulti.knownUsers.add(new user(newId, "", ""));
+
+                                System.out.println("minha lista é: " + myMulti.knownUsers);
+
+                            }
+
+                        }
+                    }
+
+                }
+                clientSocketList.clear();
+
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(listenerUnicast.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+
+        
 
     }
 
